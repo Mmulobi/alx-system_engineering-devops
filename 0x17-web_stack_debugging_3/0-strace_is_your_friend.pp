de2e75807849a1b82be1cf4a3b178ed1427e7b00
Mmulobi <mosesmutush7@gmail.com>
2023-08-09 13:49:12 -0700

    web stack debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#This script IS USED  to fix typo in config of Apache
+
+exec { 'fix config typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
